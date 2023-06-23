184274c6adcffd70f08070f02612c2fa9d078855
ailusa1 <apollo.ailus@aalto.fi>
2023-06-23 16:15:22 +0300

    Did 1.7

--- part1/unicafe/part1/src/App.js
+++ part1/unicafe/part1/src/App.js
@@ -8,13 +8,23 @@ const Display = props => <h1>{props.value}</h1>
   </button>
 )*/
 
+const Average = (props) =>{
+  let average = (1*props.g + 0*props.n + (-1)*props.b)/props.size
+  console.log('average rating:', average)
+  return <p>average {average}</p>
+}
 
+const Positive = (props) =>{
+  let pos = (props.numGood)/props.size
+  return <p>positive {pos*100} %</p>
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
 
   const handleGood = () =>{
     const updatedGood = good+1
@@ -32,6 +42,8 @@ const App = () => {
     console.log('rating: bad. times entered:', updatedBad)
   }
 
+  
+
   return (
     <div>
       <Display value = 'give feedback'/>
@@ -43,6 +55,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good+bad+neutral}</p>
+      <Average g={good} n={neutral} b={bad} size={bad+good+neutral}/>
+      <Positive numGood={good} size={bad+good+neutral}/>
     </div>
   )
 }
