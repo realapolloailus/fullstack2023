eff0486e4bf94c5a79f248103ee0106d114059d3
realapolloailus <apolloailus@gmail.com>
2023-08-23 12:18:24 +0300

    Did 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,6 +6,16 @@ const totalLikes = (blogs) => {
 	return blogs.reduce( (totalLikes, b) => totalLikes+b.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	const sortedBlogs = blogs.sort( (a, b) => a.likes > b.likes ? -1 : 1 )
+	const mostLiked = sortedBlogs[0]
+	return{
+		"title": mostLiked.title,
+		"author": mostLiked.author,
+		"likes": mostLiked.likes
+	}
+}
+
 module.exports = {
-	dummy, totalLikes
+	dummy, totalLikes, favoriteBlog
 }
