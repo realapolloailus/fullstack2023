685a3c05c8526e1eaa4d054094ff0e12c045e72b
realapolloailus <apolloailus@gmail.com>
2023-08-23 09:48:27 +0300

    Did 4.1 & 4.2

---
+++ part4/bloglist/utils/config.js
@@ -0,0 +1,9 @@
+//require('dotenv').config()
+
+const PORT = process.env.PORT
+//const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI ='mongodb+srv://realapolloailus:qvkp1555hsMy8BG2@cluster0.ogmrlge.mongodb.net/?retryWrites=true&w=majority'
+module.exports = {
+  MONGODB_URI,
+  PORT
+}
