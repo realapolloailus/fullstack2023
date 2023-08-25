410de095486f8b0771c7091b7adb07ede4a30267
realapolloailus <apolloailus@gmail.com>
2023-08-25 16:26:57 +0300

    4.12

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,40 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 
-blogsRouter.get("/", (request, response) => {
-	Blog
+blogsRouter.get("/", async(request, response) => {
+	const blogs = await Blog
 		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
+	response.json(blogs.map((blog) => blog.toJSON()))
 })
 
-blogsRouter.post("/", (request, response) => {
-	const blog = new Blog(request.body)
+blogsRouter.post("/", async(request, response) => {
+	//const blog = new Blog(request.body)
+	const body = request.body
 
-	blog
+	/*blog
 		.save()
 		.then(result => {
 			response.status(201).json(result)
-		})
+		})*/
+	
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes
+	})
+
+	if(!blog.title && !blog.url){
+		response.status(400).end()
+	}
+	else{
+		if(!blog.likes){
+			blog.likes = 0
+		}
+	}
+
+	const newBlog = await blog.save()
+	response.status(201).json(newBlog.toJSON())
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -82,9 +82,9 @@ const retrieveBlogs = async () =>{ //helper function to retrieve blogs from db
   return blogsFound.map(b => b.toJSON())
 }
 
-describe("GET request for blogs API:", () => {
+describe("Verify that GET requests work as intended:", () => {
 
-    test("blogs are returned as json", async () => {
+  test("blogs are returned as json", async () => {
         await api
             .get("/api/blogs")
             .expect(200)
@@ -97,29 +97,17 @@ describe("GET request for blogs API:", () => {
         expect(response.body).toHaveLength(blogs.length)
     }, 100000)
 
-    /*test("the first blog is about a day in the life of Joe Schmoe", async () => {
-        const response = await api.get("/api/blogs")
-    
-        expect(response.body[0].content).toBe("Day in the Life of Joe Schmoe")
-    }, 100000)
-
-    test("a specific blog is within the returned blogs", async () => {
-        const response = await api.get("/api/blogs")
-      
-      
-        const contents = response.body.map(r => r.content)
-        expect(contents).toContain(
-          "Canonical string reduction"
-        )
-      }, 100000)*/
-})
-
-describe("Check that each blog has an ID", () => {
     test("that database uses 'id' instead of '_id'", async() =>{
-        const response = await api
-            .get("/api/blogs")
-        expect(response.body[0].id).toBeDefined()
-    })
+      const response = await api
+          .get("/api/blogs")
+      expect(response.body[0].id).toBeDefined()
+  })
+
+  /*test("that there are posts with no likes in the database", async()=>{
+    const blogsToInspect = retrieveBlogs()
+    const numLikes = blogsToInspect.map(b => b.likes)
+    expect(numLikes).toContain(0)
+  })*/
 })
 
 describe("Verify that POST requests work as intended:", () =>{
@@ -149,16 +137,20 @@ describe("Verify that POST requests work as intended:", () =>{
       .send(blogToAdd)
       .expect(201)
   })
-})
 
-describe("Check that posts with 0 likes exist", () =>{
-  test("that there are posts with no likes in the database", async()=>{
-    const blogsToInspect = retrieveBlogs()
-    const numLikes = blogsToInspect.map(b => b.likes)
-    expect(numLikes).toContain(0)
+  test("that entries without title and url can be sent", async()=>{
+
+    const blogToAdd ={
+      author: "John Smith",
+      likes: "8",
+    }
+
+    await api
+      .post("/api/blogs")
+      .send(blogToAdd)
+      .expect(400)
   })
 })
-
 
 afterAll(async () => {
 	await mongoose.connection.close()
