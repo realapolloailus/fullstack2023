f0a882c1b9da3d63ef10af6a5ba7a7c756d079db
Apollo Ailus <apolloailus@gmail.com>
2024-01-07 13:29:25 +0200

    Did all tasks up through 4.20

--- part4/bloglist/utils/logger.js
+++ part4/bloglist/utils/logger.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-undef */
 const info = (...params) => {
 
-if (process.env.NODE_ENV !== "test") { 
-	console.log(...params)
-	}
-  }
-  
-  const error = (...params) => {
-  
-if (process.env.NODE_ENV !== "test") { 
-	console.error(...params)
-	}
-  }
-  
-  module.exports = {
-	info, error
-  }
+  if (process.env.NODE_ENV !== "test") { 
+    console.log(...params)
+    }
+    }
+    
+    const error = (...params) => {
+    
+  if (process.env.NODE_ENV !== "test") { 
+    console.error(...params)
+    }
+    }
+    
+    module.exports = {
+    info, error
+    }
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,4 +1,7 @@
 const logger = require("./logger")
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
+require("dotenv").config()
 
 const requestLogger = (request, response, next) => {
 	logger.info("Method:", request.method)
@@ -8,6 +11,23 @@ const requestLogger = (request, response, next) => {
 	next()
 }
 
+const userExtractor = async (request, response, next) => {
+	const decodedToken = jwt.verify(request.token, process.env.SECRET)
+	if(!request.token || !decodedToken.id){
+		return response.status(401).json({ error: "Error: Missing or invalid token." })
+	}
+	request.user = await User.findById(decodedToken.id)
+	next()
+}
+
+const tokenExtractor = async(request, response, next) => {
+	// code that extracts the token
+    const auth = await request.get("authorization")
+	if(auth && auth.toLowerCase().startsWith("bearer")){
+		request.token = auth.substring(7)
+	}
+	next()
+  }
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: "unknown endpoint" })
 }
@@ -16,9 +36,13 @@ const errorHandler = (error, request, response, next) => {
 	logger.error(error.message)
 
 	if (error.name === "CastError") {
-		return response.status(400).send({ error: "malformatted id" })
+		return response.status(400).send({ error: "Error: malformatted id" })
 	} else if (error.name === "ValidationError") {
 		return response.status(400).json({ error: error.message })
+	} else if (error.name === "JsonWebTokenError") {
+		return response.status(401).json({ error: "Error: invalid token" })
+	} else if (error.name === "TokenExpiredError") {
+		return response.status(401).json({ error: "Error: token is expired." })
 	}
 
 	next(error)
@@ -26,6 +50,8 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
 	requestLogger,
+	userExtractor,
+	tokenExtractor,
 	unknownEndpoint,
 	errorHandler
 }
