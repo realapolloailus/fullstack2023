171bb9f90c1665417d1c3a393c6ef3654f8e8336
Apollo Ailus <apolloailus@gmail.com>
2024-01-16 16:38:53 +0200

    Did 5.1, followed notes

---
+++ part5/bloglist-backend/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es2021": true,
+		"jest": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"plugins": [
+		"react"
+	],
+	"rules": {
+		/*"linebreak-style": [
+			"error",
+			"windows"
+		],*/
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		],
+		"eqeqeq": "error",
+		"no-console": 0,
+		"no-unused-vars": "off"
+	}
+}
